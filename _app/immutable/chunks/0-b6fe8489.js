8a2e12d3dab23bb53aa74db3a40823347df33ad8
Joakim Villo <villokodehode@gmail.com>
2023-01-29 11:47:49 +0100

    Updates

---
+++ _app/immutable/chunks/0-b6fe8489.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-eedca0c2.js";import{default as t}from"../components/layout.svelte-8f9a601d.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/2-cd77592a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-bbecca37.js";export{t as component};
